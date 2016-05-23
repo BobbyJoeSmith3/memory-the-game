2866d103cb10ad8c5c2b71c54e066d321155915a
Bobby Joe Smith III <bobbyjoe@codeforprogress.org>
2016-05-23 12:55:21 -0500

    Add turn counter

--- memory-the-game.py
+++ memory-the-game.py
@@ -3,91 +3,122 @@
 import simplegui
 import random
 
-number_of_pairs = 8
-card_list = []
-exposed = []
-card_width = 50
-exposed_pair_value = []
-exposed_pair_index = []
+DECK = []
+EXPOSED = []
+CARD_WIDTH = 50
+CARD_HEIGHT = 100
+GAME_STATE = 0
+CHOICE1_VALUE = None
+CHOICE1_INDEX = None
+CHOICE2_VALUE = None
+CHOICE2_INDEX = None
+TURNS = 0
+
 
 # helper function to initialize globals
-def new_game():
-    global state
-    state = 0
-    print card_list
-    print exposed
+def generate_cards(low_card, high_card):
+    global DECK
+    high_card = high_card + 1
+    list1 = range(low_card, high_card)
+    list2 = range(low_card, high_card)
+    DECK = list1 + list2
+    random.shuffle(DECK)
+    # create initial list of which cards are exposed
+    num_of_cards = len(DECK)
+    for card in range(num_of_cards):
+        EXPOSED.append(False)
+    # DELETE PRINT STATEMENTS AFTER DEBUGGING
+    print(DECK)
+    print(EXPOSED)
+    print(len(EXPOSED))
+    return DECK
 
-def generate_deck(number_of_pairs):
-    #create and concatenate two lists of numbers range 0-8
-    global card_list, exposed
-    card_list = range(number_of_pairs)
-    card_list.extend(range(number_of_pairs))
-    #shuffle deck
-    random.shuffle(card_list)
-    #create exposed card list
-    i = 0
-    while i < (number_of_pairs * 2):
-       exposed.append(False)
-       i += 1
+
+def new_game():
+    generate_cards(0,7)
+
 
 # define event handlers
 def mouseclick(pos):
-    global exposed, state, exposed_pair_value, exposed_pair_index, card_list
-    ndx = pos[0] // card_width
-    print ndx
-    # add game state logic here
-    if exposed[ndx] != True:
-        if state == 0:
-            state = 1
-        elif state == 1:
-            state = 2
-        else:
-            state = 1
+    global EXPOSED, GAME_STATE, TURNS
+    global CHOICE1_VALUE, CHOICE2_VALUE, CHOICE1_INDEX, CHOICE2_INDEX
+    #FIRST_CHOICE_VALUE, FIRST_CHOICE_INDEX, CHOICE1_VALUE, CHOICE2_VALUE, CHOICE1_INDEX, CHOICE2_INDEX
+    # determine which card was clicked
+    card_idx = pos[0] // CARD_WIDTH
 
-    if exposed[ndx] != True:
-        if state == 1:
-            exposed_pair_value[0] = card_list[ndx]
-            exposed_pair_index[0] = ndx
-            exposed[ndx] = True
-        if state == 2:
-            exposed_pair_value[1] = card_list[ndx]
-            exposed_pair_index[1] = ndx
-            exposed[ndx] = True
-            if exposed_pair_value[0] != exposed_pair_value[1]:
-                exposed[exposed_pair_index[0]] = False
-                exposed[exposed_pair_index[1]] = False
-    print "state = " + str(state)
-    exposed[ndx] = True
+    # add game state logic here
+    # only change game state if card isn't already exposed
+    if GAME_STATE == 0:
+        # flip card
+        EXPOSED[card_idx] = True
+        # assign card value to choice 1
+        CHOICE1_VALUE = DECK[card_idx]
+        # temporarily store card index of choice 1
+        CHOICE1_INDEX = card_idx
+        # change game state
+        GAME_STATE = 1
+    elif GAME_STATE == 1 and EXPOSED[card_idx] == False:
+        # flip card
+        EXPOSED[card_idx] = True
+        # assign card value to choice 2
+        CHOICE2_VALUE = DECK[card_idx]
+        # temporarily store card index of choice 2
+        CHOICE2_INDEX = card_idx
+        # change game state
+        GAME_STATE = 2
+    else:
+        # increment turn counter
+        TURNS = TURNS + 1
+        label.set_text("Turns = " + str(TURNS))
+        # check if cards are the same, if not flip
+        if CHOICE1_VALUE != CHOICE2_VALUE:
+            # flip cards back over
+            EXPOSED[CHOICE1_INDEX] = False
+            EXPOSED[CHOICE2_INDEX] = False
+        # flip card
+        EXPOSED[card_idx] = True
+        # assign card value to choice 1
+        CHOICE1_VALUE = DECK[card_idx]
+        # temporarily store card index of choice 1
+        CHOICE1_INDEX = card_idx
+        # change game state
+        GAME_STATE = 1
 
 
 # cards are logically 50x100 pixels in size
 def draw(canvas):
-    global card_width
-    card_start = 0
-    card_width = 50
-    #draw cards
-    for card in range(len(card_list)):
-        canvas.draw_polygon([(card_start,0), ((card_start + card_width),0),
-                       ((card_start + card_width),100), (card_start,100)],
-                       1, 'black', 'green')
-        #draw the number of the card if the card is exposed
-        if exposed[card] == True:
-            canvas.draw_text(str(card_list[card]), [card_start, 24], 24, "White")
-        #move over a space to draw the next card
-        card_start += card_width
+    # variables for card construction
+    text_x = 18
+    text_y = 60
+    font_size = 24
+    card_index = 0
+    card_x = 0
+    card_y = 0
+    # draw cards
+    for card_value in DECK:
+        canvas.draw_polygon([(card_x, card_y),
+                             (card_x + CARD_WIDTH, card_y),
+                             (card_x + CARD_WIDTH, card_y + CARD_HEIGHT),
+                             (card_x, card_y + CARD_HEIGHT)], 1, 'white', 'green')
+        # if card is exposed draw value, otherwise show green card without value
+        if EXPOSED[card_index] == True:
+            canvas.draw_text(str(card_value),(text_x, text_y), font_size, 'white')
+
+        card_index = card_index + 1
+        text_x = text_x + CARD_WIDTH
+        card_x = card_x + CARD_WIDTH
 
 
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
 frame.add_button("Reset", new_game)
-label = frame.add_label("Turns = 0")
+label = frame.add_label("Turns = " + str(TURNS))
 
 # register event handlers
 frame.set_mouseclick_handler(mouseclick)
 frame.set_draw_handler(draw)
 
 # get things rolling
-generate_deck(number_of_pairs)
 new_game()
 frame.start()
 
